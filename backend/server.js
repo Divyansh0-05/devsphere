167e127c62bff8c5d76c69203d7f586c1e442a64
divyansh <divyansh.singh9600@gmail.com>
2026-05-24 00:42:14 +0530

    Fix socket CORS for AWS deployment

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ const SOCKET_CORS_ORIGINS = [
   'http://127.0.0.1:5000',
   'http://localhost:5500',
   'http://127.0.0.1:5500',
+  'http://16.171.162.80:3000',
 ];
 
 const isSocketOriginAllowed = (origin) => {
